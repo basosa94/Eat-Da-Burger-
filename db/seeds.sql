1f2c33fcd75116ce6ada4fb0b2aa943f4a9e54a3
basosa94 <basosa@ucdavis.edu>
2018-06-07 09:36:27 -0700

    Add js, handlebars, and css files for functionality and style

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper jr.', true);
